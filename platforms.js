1a6287c673b9cd3bcda4ebb04b162a6f9562145b
Aaron Eldridge <aceldridge7@gmail.com>
2022-02-16 14:46:35 -0800

    adding springs back

--- platforms.js
+++ platforms.js
@@ -50,7 +50,24 @@ class Coin {
 	draw(ctx) {
 		ctx.drawImage(this.spritesheet, this.x - this.game.camera.x, this.y, 64, 64);
 	}
-}
+};
+
+class Spring {
+	constructor(game, x, y) {
+		Object.assign(this, { game, x, y});
+		
+		this.spritesheet = ASSET_MANAGER.getAsset("./sprites/Spring.png");
+		this.BB = new BoundingBox(this.x,this.y,64,64);
+	};
+	
+	update() {
+		
+	};
+	
+	draw(ctx) {
+		ctx.drawImage(this.spritesheet,this.x-this.game.camera.x,this.y,64,64);
+	};
+};
 
 class Spike{
 	constructor(game, x, y) {
