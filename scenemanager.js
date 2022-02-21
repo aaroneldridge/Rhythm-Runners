f2868839ba593e4c6d6b31272a2e951778eeac85
Merit Mamo <meritayele@gmail.com>
2022-02-21 10:44:18 -0800

    fixed restart button

--- scenemanager.js
+++ scenemanager.js
@@ -718,7 +718,7 @@ class SceneManager {
 			ctx.fillText("Restart?", 400, 600);
 		}
 		
-		if (this.flag) {
+		if (this.flag && !this.transition) {
 			
 			
 			ctx.drawImage(this.titleBackground, 0, 0, 620, 349, 0, 0, 1024, 768);
