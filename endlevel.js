c99eea50cc8f691b3a2b7f1b4b3f8fbd15cfd5cb
Calbeano <calvinisop@yahoo.com>
2022-02-24 20:15:58 -0800

    added entities to levels.js + added correct stage/level complete display

--- endlevel.js
+++ endlevel.js
@@ -5,18 +5,32 @@ class EndLevel {
 	}
 	
 	update() {
-
+		
 	}
 	
 	draw(ctx) {
 		ctx.drawImage(this.titleBackground, 0, 0, 620, 349, 0, 0, 1024, 768);
-			
+		
 		// To show completion
 		ctx.fillStyle = "Black";
-		ctx.fillText("Level One completed!", 300, 300);
+		if (this.game.camera.levelCount === 1) {
+			ctx.fillText("Stage 1-1 completed!", 300, 300);
+		}
+		
+		if (this.game.camera.levelCount === 2) {
+			ctx.fillText("Stage 1-2 completed!", 300, 300);
+		}
+		
+		if (this.game.camera.levelCount === 3) {
+			ctx.fillText("Stage 2-1 completed!", 300, 300);
+		}
+		
+		if (this.game.camera.levelCount === 4) {
+			ctx.fillText("Stage 2-2 completed!", 300, 300);
+		}
 			
 		// To go back to the beginning
-		ctx.fillStyle = this.game.mouse && this.game.mouse.x > 400 && this.game.mouse.x < 625 && this.game.mouse.y > 560 && this.game.mouse.y < 610 ? "White" : "Black";
-		ctx.fillText("Restart?", 400, 600);
+		ctx.fillStyle = this.game.mouse && this.game.mouse.x > 390 && this.game.mouse.x < 650 && this.game.mouse.y > 660 && this.game.mouse.y < 710 ? "White" : "Black";
+		ctx.fillText("Continue", 400, 700);
 	}
 }
